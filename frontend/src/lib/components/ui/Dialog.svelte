7908b622097f2bd5d6908a658e1b00124fb88ad2
mori1704 <1704mori@gmail.com>
2023-12-22 15:07:01 -0300

    Add footer slot to dialog

--- frontend/src/lib/components/ui/Dialog.svelte
+++ frontend/src/lib/components/ui/Dialog.svelte
@@ -1,60 +1,61 @@
 <script lang="ts">
-	export let showModal = false;
+  export let showModal = false;
 
-	let dialog: HTMLDialogElement;
+  let dialog: HTMLDialogElement;
 
-	$: if (dialog && showModal) dialog.showModal();
+  $: if (dialog && showModal) dialog.showModal();
 </script>
 
 <!-- svelte-ignore a11y-click-events-have-key-events a11y-no-noninteractive-element-interactions -->
 <dialog
-	bind:this={dialog}
-	on:close={() => (showModal = false)}
-	on:click|self={() => dialog.close()}
-	class="max-w-2xl rounded-md border border-neutral-600 dark:border-neutral-800 p-0 bg-white dark:bg-neutral-950 dark:text-neutral-100"
+  bind:this={dialog}
+  on:close={() => (showModal = false)}
+  on:click|self={() => dialog.close()}
+  class="max-w-2xl rounded-md border border-neutral-600 dark:border-neutral-800 p-0 bg-white dark:bg-neutral-950 dark:text-neutral-100"
 >
-	<!-- svelte-ignore a11y-no-static-element-interactions -->
-	<div
-		on:click|stopPropagation
-		class="p-4"
-	>
-		<slot name="header" />
-		<hr />
-		<slot />
-		<hr />
-		<!-- svelte-ignore a11y-autofocus -->
-		<button autofocus on:click={() => dialog.close()}>close modal</button>
-	</div>
+  <!-- svelte-ignore a11y-no-static-element-interactions -->
+  <div on:click|stopPropagation class="flex flex-col gap-4 p-4">
+    <slot name="header" />
+    <slot />
+    <!-- svelte-ignore a11y-autofocus -->
+    <div class="flex items-center gap-2 self-end">
+      <slot name="footer">
+        <button class="hover:underline" on:click={() => dialog.close()}>
+          Close
+        </button>
+      </slot>
+    </div>
+  </div>
 </dialog>
 
 <style>
-	dialog::backdrop {
-		background-color: rgba(0, 0, 0, 0.3);
-		backdrop-filter: blur(5px);
-	}
-	dialog[open] {
-		animation: zoom 0.3s cubic-bezier(0.34, 1.56, 0.64, 1);
-	}
-	@keyframes zoom {
-		from {
-			transform: scale(0.95);
-		}
-		to {
-			transform: scale(1);
-		}
-	}
-	dialog[open]::backdrop {
-		animation: fade 0.2s ease-out;
-	}
-	@keyframes fade {
-		from {
-			opacity: 0;
-		}
-		to {
-			opacity: 1;
-		}
-	}
-	button {
-		display: block;
-	}
+  dialog::backdrop {
+    background-color: rgba(0, 0, 0, 0.3);
+    backdrop-filter: blur(5px);
+  }
+  dialog[open] {
+    animation: zoom 0.3s cubic-bezier(0.34, 1.56, 0.64, 1);
+  }
+  @keyframes zoom {
+    from {
+      transform: scale(0.95);
+    }
+    to {
+      transform: scale(1);
+    }
+  }
+  dialog[open]::backdrop {
+    animation: fade 0.2s ease-out;
+  }
+  @keyframes fade {
+    from {
+      opacity: 0;
+    }
+    to {
+      opacity: 1;
+    }
+  }
+  button {
+    display: block;
+  }
 </style>
