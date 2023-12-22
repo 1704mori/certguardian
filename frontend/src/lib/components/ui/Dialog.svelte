ef069e19b8070cf59cb3761985dfd420eeaa4ccf
mori1704 <1704mori@gmail.com>
2023-12-22 15:02:35 -0300

    Add Dialog component

---
+++ frontend/src/lib/components/ui/Dialog.svelte
@@ -0,0 +1,60 @@
+<script lang="ts">
+	export let showModal = false;
+
+	let dialog: HTMLDialogElement;
+
+	$: if (dialog && showModal) dialog.showModal();
+</script>
+
+<!-- svelte-ignore a11y-click-events-have-key-events a11y-no-noninteractive-element-interactions -->
+<dialog
+	bind:this={dialog}
+	on:close={() => (showModal = false)}
+	on:click|self={() => dialog.close()}
+	class="max-w-2xl rounded-md border border-neutral-600 dark:border-neutral-800 p-0 bg-white dark:bg-neutral-950 dark:text-neutral-100"
+>
+	<!-- svelte-ignore a11y-no-static-element-interactions -->
+	<div
+		on:click|stopPropagation
+		class="p-4"
+	>
+		<slot name="header" />
+		<hr />
+		<slot />
+		<hr />
+		<!-- svelte-ignore a11y-autofocus -->
+		<button autofocus on:click={() => dialog.close()}>close modal</button>
+	</div>
+</dialog>
+
+<style>
+	dialog::backdrop {
+		background-color: rgba(0, 0, 0, 0.3);
+		backdrop-filter: blur(5px);
+	}
+	dialog[open] {
+		animation: zoom 0.3s cubic-bezier(0.34, 1.56, 0.64, 1);
+	}
+	@keyframes zoom {
+		from {
+			transform: scale(0.95);
+		}
+		to {
+			transform: scale(1);
+		}
+	}
+	dialog[open]::backdrop {
+		animation: fade 0.2s ease-out;
+	}
+	@keyframes fade {
+		from {
+			opacity: 0;
+		}
+		to {
+			opacity: 1;
+		}
+	}
+	button {
+		display: block;
+	}
+</style>
