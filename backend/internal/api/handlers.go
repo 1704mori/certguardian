597321b1703df654032aba36de1155d7bc842495
mori1704 <1704mori@gmail.com>
2023-12-22 18:12:03 -0300

    Add domain CRUD operations to API handlers

--- backend/internal/api/handlers.go
+++ backend/internal/api/handlers.go
@@ -1,20 +1,68 @@
 package api
 
 import (
+	"net/http"
+
+	"github.com/1704mori/certguardian/internal/certificate"
 	"github.com/1704mori/certguardian/internal/db"
+	"github.com/1704mori/certguardian/internal/repository"
 	"github.com/gin-gonic/gin"
 )
 
 type Handler struct {
-	DB *db.Database
+	DB   *db.Database
+	repo *repository.CertificateRepository
 }
 
-func NewHandler(db *db.Database) *Handler {
-	return &Handler{DB: db}
+func NewHandler(
+	db *db.Database,
+	repo *repository.CertificateRepository,
+) *Handler {
+	return &Handler{
+		DB:   db,
+		repo: repo,
+	}
 }
 
 func (h *Handler) addDomain(c *gin.Context) {
-	c.JSON(200, gin.H{"message": "todo"})
+	var domain certificate.Metadata
+
+	if err := c.ShouldBindJSON(&domain); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	if d, _ := h.repo.Find(domain.CommonName); d != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Domain already exists"})
+		return
+	}
+
+	err := h.repo.Add(domain.CommonName, certificate.Metadata{
+		CommonName: domain.CommonName,
+	})
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "Domain added successfully"})
+}
+
+func (h *Handler) findDomain(c *gin.Context) {
+	domain := c.Param("domain")
+
+	if domain == "" {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid domain"})
+		return
+	}
+
+	d, err := h.repo.Find(domain)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, d)
 }
 
 func (h *Handler) listDomain(c *gin.Context) {
--- backend/internal/api/routes.go
+++ backend/internal/api/routes.go
@@ -2,11 +2,15 @@ package api
 
 import (
 	"github.com/1704mori/certguardian/internal/db"
+	"github.com/1704mori/certguardian/internal/repository"
 	"github.com/gin-gonic/gin"
 )
 
 func setupRoutes(router *gin.RouterGroup, db *db.Database) {
-	h := NewHandler(db)
+	certRepo := repository.NewCertificateRepository(db)
+	h := NewHandler(db, certRepo)
 
 	router.GET("/", h.listDomain)
+	router.GET("/:domain", h.findDomain)
+	router.POST("/", h.addDomain)
 }
