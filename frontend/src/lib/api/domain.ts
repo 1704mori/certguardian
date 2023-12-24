ecd432a04a8007f191d096a883ea9326b242d51e
mori1704 <1704mori@gmail.com>
2023-12-23 23:07:43 -0300

    Refactor domain api

--- frontend/src/lib/api/domain.ts
+++ frontend/src/lib/api/domain.ts
@@ -1,8 +1,10 @@
-type Response<T> = [T, Error | null];
-type ApiResponse = { message: string | any; error: string; };
+import { host } from "$lib/env";
 
-export async function addDomain(domain: string): Promise<Response<ApiResponse>> {
-  const response = await fetch(`http://localhost:7070/v1/`, {
+type ApiResponse<T = any> = { message: T; error?: string; };
+export type DomainList = { commonName: string; issuer: string; validFrom: string; validTo: string; isExpired: boolean };
+
+async function addDomain(domain: string): Promise<ApiResponse<string>> {
+  const response = await fetch(`${host()}/v1/domain`, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
@@ -10,12 +12,56 @@ export async function addDomain(domain: string): Promise<Response<ApiResponse>> 
     body: JSON.stringify({ commonName: domain }),
   });
 
-  const data: ApiResponse = await response.json();
+  const data: ApiResponse<string> = await response.json();
 
   if (response.status !== 200) {
     console.error(data);
-    return [null as any, new Error(data.error)];
+    // return [null as any, new Error(data.error)];
+    throw new Error(data.error);
   }
 
-  return [data, null];
+  return data;
+}
+
+async function listDomains(): Promise<ApiResponse<DomainList[]>> {
+  const response = await fetch(`${host()}/v1/domain`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+
+  const data: ApiResponse<DomainList[]> = await response.json();
+
+  if (response.status !== 200) {
+    console.error(data);
+    // return [null as any, new Error(data.error)];
+    throw new Error(data.error);
+  }
+
+  return data;
+}
+
+async function deleteDomain(domain: string): Promise<ApiResponse<string>> {
+  const response = await fetch(`${host()}/v1/domain/${domain}`, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+
+  const data: ApiResponse<string> = await response.json();
+
+  if (response.status !== 200) {
+    console.error(data);
+    throw new Error(data.error);
+  }
+
+  return data
+}
+
+export {
+  addDomain,
+  listDomains,
+  deleteDomain,
 }
