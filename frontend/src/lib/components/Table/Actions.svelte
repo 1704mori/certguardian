48ad06e95c5d2b3498ffa769886d4485cb78d7e8
mori1704 <1704mori@gmail.com>
2023-12-23 23:08:46 -0300

    Add Table actions component

---
+++ frontend/src/lib/components/Table/Actions.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import type { DomainList } from "$lib/api/domain";
+  import domain from "$lib/stores/domain";
+  import { Trash } from "lucide-svelte";
+  import { toast } from "svelte-sonner";
+
+  export let data: DomainList;
+
+  async function handleDelete() {
+    toast.promise(domain.deleteDomain(data.commonName), {
+      success: (d) => d.message,
+      error: (e: any) => e.message ?? "Something went wrong",
+    });
+  }
+</script>
+
+<div class="flex items-center justify-center gap-2">
+  <button type="button" on:click={handleDelete}>
+    <Trash />
+  </button>
+</div>
