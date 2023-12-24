a7de2bb79ddb6431d16c31ebfe9d2b9c4f2639ac
mori1704 <1704mori@gmail.com>
2023-12-24 19:32:32 -0300

    WIP AddCertificate

---
+++ frontend/src/lib/components/AddCertificate.svelte
@@ -0,0 +1,118 @@
+<script lang="ts">
+  import domain from "$lib/stores/domain";
+  import Button from "./ui/Button.svelte";
+  import Dialog from "./ui/Dialog.svelte";
+  import Input from "./ui/Input.svelte";
+  import { toast } from "svelte-sonner";
+  import Icon from "./ui/icons/Icon.svelte";
+  import Trash from "./ui/icons/Trash.svelte";
+
+  let _dirs: HTMLSpanElement[] = [];
+  let dirs: string[] = [];
+
+  let showModal = false;
+  let dir = "";
+
+  $: isValidDir = dir.trim() && !dirs.includes(dir.trim());
+  $: console.log(_dirs);
+
+  let loading = false;
+
+  function addDir() {
+    if (!dir.trim()) return;
+    if (dirs.includes(dir)) return;
+
+    dirs = [...dirs, dir.trim()];
+    dir = "";
+  }
+
+  function removeDir(index: number) {
+    dirs = dirs.slice(0, index).concat(dirs.slice(index + 1));
+    _dirs = _dirs.slice(0, index).concat(_dirs.slice(index + 1));
+  }
+
+  function getDimensions(index: number) {
+    return _dirs?.[index].getBoundingClientRect();
+  }
+
+  async function handleSubmit() {
+    return;
+    loading = true;
+    toast.promise(domain.addNewDomain(dir), {
+      success: (d) => d.message,
+      error: (e: any) => e.message ?? "Something went wrong",
+    });
+
+    loading = false;
+    showModal = false;
+  }
+</script>
+
+<Dialog
+  class="min-w-[32rem] max-w-[32rem]"
+  bind:showModal
+  on:submit={handleSubmit}
+>
+  <h2 slot="header">Add Certificate</h2>
+
+  <div class="flex flex-col">
+    <div class="flex flex-col w-full gap-1.5">
+      <label class="font-medium text-xs uppercase" for="dir">Directory</label>
+      <Input
+        id="dir"
+        placeholder="e.g: /etc/letsencrypt/live/example.com"
+        bind:value={dir}
+        on:keydown={(e) => e.key == "Enter" && addDir()}
+        on:blur={addDir}
+      />
+    </div>
+    {#if isValidDir}
+      <div
+        class="flex items-center flex-wrap gap-2 bg-neutral-100 dark:bg-neutral-900 rounded-md p-1 w-full opacity-60"
+      >
+        <div
+          class="bg-white dark:bg-black border border-neutral-800 dark:border-neutral-800 hover:border-neutral-500 dark:hover:border-neutral-600 rounded-md p-1 break-words max-w-[32rem] truncate"
+        >
+          {dir}
+        </div>
+      </div>
+      <span class="text-xs text-neutral-700 dark:text-neutral-500 mt-1">
+        Press 'Enter' or click outside the input to add a directory
+      </span>
+    {/if}
+  </div>
+  {#if dirs.length}
+    <div
+      class="flex items-center flex-1 flex-wrap overflow-hidden gap-2 bg-neutral-100 dark:bg-neutral-900 rounded-md p-1 w-full"
+    >
+      {#each dirs as dir, index}
+        <button
+          type="button"
+          class="inline-flex items-center gap-2 bg-white dark:bg-black border border-neutral-800 dark:border-neutral-800 hover:border-neutral-500 dark:hover:border-neutral-600 rounded-md p-1 group max-h-8 min-h-8 cursor-pointer max-w-[32rem] truncate"
+          on:click={() => removeDir(index)}
+        >
+          <span class="max-w-[32rem] truncate" bind:this={_dirs[index]}>
+            {dir}
+          </span>
+          <button type="button"> X </button>
+          <!-- {#if _dirs.length && _dirs[index]}
+            <span
+              class="hidden group-hover:flex justify-center items-center"
+              style={`width: ${getDimensions(index)?.width}px; height: ${
+                getDimensions(index)?.height
+              }px`}
+            >
+              <Icon size="16">
+                <Trash />
+              </Icon>
+            </span>
+          {/if} -->
+        </button>
+      {/each}
+    </div>
+  {/if}
+
+  <Button type="button" slot="footer" {loading}>Add</Button>
+</Dialog>
+
+<Button on:click={() => (showModal = true)}>Add Directory</Button>
