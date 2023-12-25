f0b240ec750d8c858dcb0d788b0fa8de7cc5eba9
mori1704 <1704mori@gmail.com>
2023-12-24 21:33:20 -0300

    Refactor certificates repository

--- backend/internal/repository/certificates.go
+++ backend/internal/repository/certificates.go
@@ -16,8 +16,8 @@ func NewCertificateRepository(database *db.Database) *CertificateRepository {
 	return &CertificateRepository{DB: database}
 }
 
-func (repo *CertificateRepository) Add(certsDir []string) error {
-	jsonBytes, err := json.Marshal(certsDir)
+func (repo *CertificateRepository) Add(info certificates.Info) error {
+	jsonBytes, err := json.Marshal(info)
 	if err != nil {
 		return err
 	}
@@ -32,26 +32,6 @@ func (repo *CertificateRepository) Add(certsDir []string) error {
 	})
 
 	return err
-}
-
-func (repo *CertificateRepository) Find() (certificates.Info, error) {
-	var certs certificates.Info
-
-	err := repo.DB.DB.View(func(tx *bbolt.Tx) error {
-		bucket := tx.Bucket([]byte("Certificates"))
-		if bucket == nil {
-			return bbolt.ErrBucketNotFound
-		}
-
-		certsDirBytes := bucket.Get([]byte("directories"))
-		if certsDirBytes == nil {
-			return bbolt.ErrInvalid
-		}
-
-		return json.Unmarshal(certsDirBytes, &certs)
-	})
-
-	return certs, err
 }
 
 func (repo *CertificateRepository) FindByDirectories(directories []string) (certificates.Info, error) {
@@ -72,4 +52,30 @@ func (repo *CertificateRepository) FindByDirectories(directories []string) (cert
 	})
 
 	return certs, err
+}
+
+func (repo *CertificateRepository) List() ([]certificates.Info, error) {
+	var domains []certificates.Info
+
+	err := repo.DB.DB.View(func(tx *bbolt.Tx) error {
+		bucket := tx.Bucket([]byte("Certificates"))
+		if bucket == nil {
+			return bbolt.ErrBucketNotFound
+		}
+
+		return bucket.ForEach(func(k, v []byte) error {
+			var domainMeta certificates.Info
+
+			err := json.Unmarshal(v, &domainMeta)
+			if err != nil {
+				return err
+			}
+
+			domains = append(domains, domainMeta)
+
+			return nil
+		})
+	})
+
+	return domains, err
 }
