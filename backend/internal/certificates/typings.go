483c15871ea790947717805fc0e79df162d3e256
mori1704 <1704mori@gmail.com>
2023-12-24 21:33:29 -0300

    Update certificates typings

--- backend/internal/certificates/typings.go
+++ backend/internal/certificates/typings.go
@@ -1,6 +1,10 @@
 package certificates
 
+import "github.com/1704mori/certguardian/internal/domain"
+
+// type DirectoryInfo map[string][]string
+type DirectoryInfo map[string]map[string]domain.Info
+
 type Info struct {
-	Directories  []string `json:"directories" binding:"required"`
-	Certificates []string `json:"certificates"`
+	Directories DirectoryInfo `json:"directories" binding:"required"`
 }
