f80899347b5ff03f0b4ac91dee7274e8bcd1a7e4
mori1704 <1704mori@gmail.com>
2023-12-26 19:09:30 -0300

    Add form to AddDomain and some domain name validations

--- frontend/src/lib/components/AddDomain.svelte
+++ frontend/src/lib/components/AddDomain.svelte
@@ -10,7 +10,19 @@
 
   let loading = false;
 
+  const domainRegex = /^((?!-)[A-Za-z0-9-]{1,63}(?<!-)\.)+[A-Za-z]{2,}$/;
+
   async function handleSubmit() {
+    if (!_domain.trim()) {
+      toast.error("Domain cannot be empty");
+      return;
+    }
+
+    if (!domainRegex.test(_domain.trim())) {
+      toast.error("Invalid domain name");
+      return;
+    }
+
     loading = true;
     toast.promise(domain.addNewDomain(_domain), {
       success: (d) => d.message,
@@ -24,11 +36,14 @@
 
 <Dialog class="min-w-[32rem] max-w-[32rem]" bind:showModal>
   <h2 slot="header">Add Domain</h2>
+  <form on:submit|preventDefault={handleSubmit}>
+    <div class="flex flex-col w-full gap-1.5">
+      <label class="font-medium text-xs uppercase" for="domain">Domain</label>
+      <Input id="domain" placeholder="example.com" bind:value={_domain} />
+    </div>
 
-  <div class="flex flex-col w-full gap-1.5">
-    <label class="font-medium text-xs uppercase" for="domain">Domain</label>
-    <Input id="domain" placeholder="example.com" bind:value={_domain} />
-  </div>
+    <button type="submit" hidden>Add</button>
+  </form>
 
   <Button slot="footer" {loading} on:click={handleSubmit}>Add</Button>
 </Dialog>
